0f8c08801b679d133c853818b3cc3df0d9516160
Anno100 <alessmon.acoro@googlemail.com>
2024-06-27 14:33:50 +0200

    test

--- Home/Main.js
+++ Home/Main.js
@@ -1,27 +1,5 @@
 
 
 function Main(){
-    let c = createCanvas("Testing",500,500);
-    let g = c.g;
-    let Mouse = c.Mouse;
-    let v = new Vector2D(100,100,'orange');
-    let w = new Circle(20,20,50,'blue');
-    
-    c.Draw = () => {
 
-
-        w.x = Mouse.x;
-        w.y = Mouse.y;
-        
-        if(v.overlap(w)) console.log("Yes");
-        console.log(w)
-
-        g.setFillStyle("black");
-        g.fillRect(0,0,c.width,c.height);
-        v.draw(g);
-        w.draw(g);
-
-        g.setStrokeStyle('blue');
-        v.drawTo(g,w);
-    }
 }
