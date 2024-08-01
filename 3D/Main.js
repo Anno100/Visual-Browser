19343dad1bdbaafa2fa7217c9f58c8555e82b53a
Anno100 <alessmon.acoro@googlemail.com>
2024-08-01 11:58:00 +0200

    playable game

---
+++ 3D/Main.js
@@ -0,0 +1,84 @@
+class RectSBS{
+    constructor(x,y,width,height,color){
+        this.x = x;
+        this.y = y;
+        this.z = 0;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+
+    /**
+     * 
+     * @param {Canvas3D} canvas 
+     */
+    draw = (canvas) => {
+
+        canvas.fillRect(this.x,this.y,this.width,this.height,0,this.color);
+
+    }
+}
+
+function Main() {
+    console.clear();
+    createFormButton('Home');
+
+    let canvas_3d = Canvas3D.createSBSCanvas();
+
+    let M = { x: -100, y: -100 }
+    let onLeftSide = true;
+    canvas_3d.left.element.onmousemove = (e) => {
+        M.x = e.offsetX;
+        M.y = e.offsetY;
+        onLeftSide = true;
+    }
+    canvas_3d.right.element.onmousemove = (e) => {
+        M.x = e.offsetX;
+        M.y = e.offsetY;
+        onLeftSide = false;
+    }
+
+    player = canvas_3d.addGameObject(new RectSBS(50,500,5,10,'blue'));
+    right = false;
+    left = false;
+    document.body.addEventListener('keydown',(e)=>{
+        if(e.key == 'a') left = true;
+        if(e.key == 'd') right = true;
+    });
+    canvas_3d.left.element.onmousedown = () => {
+        left = true;
+    }
+    canvas_3d.left.element.onmouseup = () => {
+        left = false;
+    }
+    canvas_3d.right.element.onmousedown = () => {
+        right = true;
+    }
+    canvas_3d.right.element.onmouseup = () => {
+        right = false;
+    }
+    document.body.addEventListener('keyup',(e)=>{
+        if(e.key == 'a') left = false;
+        if(e.key == 'd') right = false;
+    });
+
+    green = canvas_3d.addGameObject(new RectSBS(0,0,2,2,'green'));
+    red = canvas_3d.addGameObject(new RectSBS(0,0,2,2,'red'));
+    canvas_3d.Draw = () => {
+            canvas_3d.clear();
+
+
+            if(left) player.x-=2;
+            if(right) player.x += 2;
+
+            if(onLeftSide){
+                red.x = M.x;
+                red.y = M.y;
+            }
+            else {
+                green.x = M.x-2;
+                green.y = M.y;
+            }
+        
+    };
+}
